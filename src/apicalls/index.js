53152e76a38266cdd41fa58eb9cc9f6d6b645d98
Nikhil Kumar <128170642+Nikhilmukheja19@users.noreply.github.com>
2025-04-27 15:05:14 +0530

    Update index.js

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://quiz-server-s473.onrender.com",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
